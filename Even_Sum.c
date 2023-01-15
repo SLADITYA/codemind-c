24750cc831a28b5fec0bd491e30d1d49f682b639
SLADITYA <119839835+SLADITYA@users.noreply.github.com>
2023-01-15 23:31:05 +0530

    #include<stdio.h> int main() {     int a,i,sum=0;     scanf("%d",&a);     int n[a];     for(i=0;i<a;i++)     {       scanf("%d",&n[i]);       if(n[i]%2==0)       sum=sum+n[i];     }     printf("%d",sum);
    }

---
+++ Even_Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,sum=0;
+    scanf("%d",&a);
+    int n[a];
+    for(i=0;i<a;i++)
+    {
+      scanf("%d",&n[i]);
+      if(n[i]%2==0)
+      sum=sum+n[i];
+    }
+    printf("%d",sum);
+    
+}
